2f19cb790cee627d5af88c12c72c044fa656fcc2
Jaylin16 <jaylin6115@gmail.com>
2023-05-17 23:38:38 +0900

    [Mybatis] studyLog CRUD기능 - mapper Dao 수정, CRUD sql구문 xml 추가 [해결]

--- src/main/java/com/spring/mybatisPrac/dao/StudyDao.java
+++ src/main/java/com/spring/mybatisPrac/dao/StudyDao.java
@@ -8,5 +8,18 @@ import java.util.List;
 @Mapper
 public interface StudyDao {
 
+    //공부기록 전체 리스트
     public List<Vo_study> doStudyList();
+
+    //공부기록 1개
+    public Vo_study getStudyLog(String keyId);
+
+    //공부기록 수정
+    public int updateStudyLog(Vo_study vo_study);
+
+    //공부기록 삭제
+    public int deleteStudyLog(String keyId);
+
+    //공부하기 등록
+    public int createStudyLog(Vo_study vo_study);
 }
